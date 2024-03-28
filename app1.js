ec271acb43119fa17d02feb7c5a634fd11352b73
Arthur <arthurfelixpereira647@gmail.com>
2024-03-28 15:28:55 -0300

    Subindo arquivo 1

---
+++ app1.js
@@ -0,0 +1,15 @@
+
+// console.log('nomes');
+
+// let pessoas = require('./dados');
+// console.log(pessoas);
+// console.log(pessoas[2]);
+
+// console.log(pessoas.nomes);
+// console.log(pessoas.frutas);
+
+//usando desestruração
+let {nomes, frutas, Soma}=require('./dados');
+console.log(nomes);
+console.log(frutas);
+console.log(Soma(5,55));
